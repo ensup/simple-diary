38b9081473f38ceef3018eee98faabdb28a0c623
ensup <ensup0805@outlook.com>
2019-05-04 13:27:33 +0900

    Fixed an error

--- diary.py
+++ diary.py
@@ -3,6 +3,6 @@ f = open('diary.txt', 'a', encoding="utf8")
 con=input('Please type the content of the dairy:')
 f.write(con)
 f.write('\n')
-f.write(t.ctime)
+f.write(t.ctime())
 f.write('\n')
 f.close
